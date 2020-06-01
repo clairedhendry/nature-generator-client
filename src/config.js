130b7c66d7e14605296e020989626ef1c68b7832
Claire Hendry <claire.d.hendry@gmail.com>
2020-06-01 11:46:10 -0400

    basic functionality

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
-    API_ENDPOINT: 'https://murmuring-dawn-57367.herokuapp.com/',
+    
     TOKEN_KEY: 'client-auth-token',
     AUTHORIZATION: process.env.REAC_APP_AUTHORIZATION,
     API_KEY: process.env.REACT_APP_API_KEY,
+    API_BASE_URL: 'https://murmuring-dawn-57367.herokuapp.com/',
 }
